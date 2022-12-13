5dff9e08fb623ab71024bf391c4ee77364110afd
Linus <linlo001@student.liu.se>
2022-12-13 12:33:53 +0100

    Merge remote-tracking branch 'origin/main' into frost

--- src/pepe.h
+++ src/pepe.h
@@ -2,6 +2,7 @@
 #define PEPE_H
 
 #include "turret.h"
+#include "projectile.h"
 #include "enemy.h"
 
 class Pepe : public Turret
@@ -9,9 +10,12 @@ class Pepe : public Turret
 private:
     void attack(sf::Time delta) override;
     float cooldown;
+    sf::Texture &projectile_texture;
 
 public:
-    Pepe(sf::Texture &texture, sf::Vector2f position, std::vector<Entity *> &entities);
+    Pepe(sf::Texture &texture, sf::Vector2f position,
+         std::vector<Entity *> &entities,
+         sf::Texture &projectile_texture);
 
     void update(sf::Time delta) override;
 };
